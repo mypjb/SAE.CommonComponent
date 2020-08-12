ea1ef0f2b675abdfd13eff765eaf0d65d9df4e68
mypjb <280417314@qq.com>
2020-08-12 22:08:57 +0800

    [Integrate] integrate oauth

--- clients/Master/src/pages/index.tsx
+++ clients/Master/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MicroApp } from 'umi'
+import { format } from 'prettier';
 
 const findApp = (pathname, routes) => {
   if (pathname && routes) {
@@ -24,11 +25,23 @@ const routeList = [{
 {
   path: '/oauth',
   microApp: 'oauth'
+},
+{
+  path: '/config-server',
+  microApp: 'config-server'
+},
+{
+  path: '/routing',
+  microApp: 'routing'
 }];
 
 export default ({ location }) => {
+
   const app = findApp(location.pathname, routeList);
+
   const element = (app) ? (<MicroApp name={app.microApp} ></MicroApp>) : (<div>404</div>);
+
+  console.log(app);
   return (
     <div>
       {element}
--- clients/Master/start.sh
+++ clients/Master/start.sh
@@ -6,6 +6,8 @@ echo start childs app
 # start childs
 cd ../Identity && tyarn start &
 cd ../OAuth && tyarn start &
+cd ../Routing && tyarn start &
+cd ../ConfigServer && tyarn start &
 
 echo start master app
 # start master 
