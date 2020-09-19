2ab319d501daf8613b5d894ccab13465191ffce3
mypjb <280417314@qq.com>
2020-09-19 15:40:24 +0800

    [Client] add env config

--- clients/Master/src/app.ts
+++ clients/Master/src/app.ts
@@ -1,9 +1,48 @@
 import { RequestConfig } from 'umi';
-import { addGlobalUncaughtErrorHandler } from 'qiankun';
 
+const ENV = process.env.NODE_ENV;
 
+let apps = [];
 
-addGlobalUncaughtErrorHandler(event => console.log(event));
+if (ENV == "development") {
+    apps = [
+        {
+            name: 'config-server', // 唯一 id
+            entry: '//dev.sae.com:8001', // html entry
+        },
+        {
+            name: 'identity', // 唯一 id
+            entry: '//dev.sae.com:8002', // html entry
+        },
+        {
+            name: 'oauth', // 唯一 id
+            entry: '//dev.sae.com:8003', // html entry
+        },
+        {
+            name: 'routing', // 唯一 id
+            entry: '//dev.sae.com:8004', // html entry
+        }
+    ];
+} else {
+    apps = [
+        {
+            name: 'config-server', // 唯一 id
+            entry: '//configserver.client.sae.com', // html entry
+        },
+        {
+            name: 'identity', // 唯一 id
+            entry: '//identity.client.sae.com', // html entry
+        },
+        {
+            name: 'oauth', // 唯一 id
+            entry: '//oauth.client.sae.com', // html entry
+        },
+        {
+            name: 'routing', // 唯一 id
+            entry: '//routing.client.sae.com', // html entry
+        }
+    ];
+}
 
 const menuData = [
     {
@@ -33,30 +72,13 @@ const menuData = [
 export const qiankun = function () {
     return {
         // 注册子应用信息
-        apps: [
-            {
-                name: 'config-server', // 唯一 id
-                entry: '//dev.sae.com:8001', // html entry
-            },
-            {
-                name: 'identity', // 唯一 id
-                entry: '//dev.sae.com:8002', // html entry
-            },
-            {
-                name: 'oauth', // 唯一 id
-                entry: '//dev.sae.com:8003', // html entry
-            },
-            {
-                name: 'routing', // 唯一 id
-                entry: '//dev.sae.com:8004', // html entry
-            }
-        ],
+        apps,
         lifeCycles: {
             afterMount: props => {
                 console.log(props);
             },
         },
-        addGlobalUncaughtErrorHandler:e => console.log(e)
+        addGlobalUncaughtErrorHandler: e => console.log(e)
     }
 };
 
