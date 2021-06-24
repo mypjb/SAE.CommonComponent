741535dd8239dd94960ee53c1301a466e82bde71
mypjb <280417314@qq.com>
2021-06-24 21:56:22 +0800

    [clients] Add env

--- clients/Master/config/app.js
+++ clients/Master/config/app.js
@@ -1,41 +1,40 @@
-const ENV = process.env.NODE_ENV;
+const ENV = process.env.UMI_ENV;
 
-console.log({ENV,UMI_ENV:process.env.UMI_ENV});
 let apps = [
     {
         name: 'config-server', // 唯一 id
         entry: '//dev.sae.com:8001', // html entry
-        path:"/config",
+        path: "/config",
     },
     {
         name: 'identity', // 唯一 id
         entry: '//dev.sae.com:8002', // html entry
-        path:"/identity",
+        path: "/identity",
 
     },
     {
         name: 'oauth', // 唯一 id
         entry: '//dev.sae.com:8003', // html entry
-        path:"/oauth",
+        path: "/oauth",
 
     },
     {
         name: 'routing', // 唯一 id
         entry: '//dev.sae.com:8004', // html entry
-        path:"/routing",
+        path: "/routing",
     },
     {
         name: 'authorize', // 唯一 id
         entry: '//dev.sae.com:8005', // html entry
-        path:"/auth",
+        path: "/auth",
     }
 ];
 
-if (ENV != "development") {
-    apps[0].entry='//configserver.client.sae.com'
-    apps[1].entry='//identity.client.sae.com'
-    apps[2].entry='//oauth.client.sae.com'
-    apps[3].entry='//routing.client.sae.com'
+if (ENV == "prod") {
+    apps[0].entry = '//configserver.client.sae.com'
+    apps[1].entry = '//identity.client.sae.com'
+    apps[2].entry = '//oauth.client.sae.com'
+    apps[3].entry = '//routing.client.sae.com'
 }
-console.log(apps);
+console.log({ENV,apps});
 export default apps;
