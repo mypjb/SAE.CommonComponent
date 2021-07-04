73013bcddb10a737918be95043af5b1524143876
小姐请留步 <280417314@qq.com>
2021-07-04 22:07:34 +0800

    [user] User register remove auth

--- src/SAE.CommonComponent.User/Controllers/UserController.cs
+++ src/SAE.CommonComponent.User/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SAE.CommonComponent.User.Abstract.Dtos;
 using SAE.CommonComponent.User.Commands;
 using SAE.CommonLibrary.Abstract.Mediator;
@@ -29,6 +30,7 @@ namespace SAE.CommonComponent.User.Controllers
             return dto;
         }
 
+        [AllowAnonymous]
         [HttpPost("{action}")]
         public async Task<object> Register(UserCommand.Register command)
         {
