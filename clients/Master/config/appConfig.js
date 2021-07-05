1793768ed405f77e35a5bb04a936d57e79c2db72
mypjb <280417314@qq.com>
2021-07-05 23:10:04 +0800

    [client] Sh add authorize

--- clients/Master/config/appConfig.js
+++ clients/Master/config/appConfig.js
@@ -1,6 +1,6 @@
 const ENV = process.env.UMI_ENV;
 
-const apps = [
+let apps = [
     {
         name: 'config-server', // 唯一 id
         entry: '//localhost:8001', // html entry
@@ -58,15 +58,15 @@ let appConfig = {
 };
 
 if (ENV == "prod") {
-    apps[0].entry = '//configserver.client.sae.com'
-    apps[1].entry = '//identity.client.sae.com'
-    apps[2].entry = '//oauth.client.sae.com'
-    apps[3].entry = '//routing.client.sae.com'
-    apps[4].entry = '//authorize.client.sae.com'
+    apps[0].entry = '//configserver.client.sae.com';
+    apps[1].entry = '//identity.client.sae.com';
+    apps[2].entry = '//oauth.client.sae.com';
+    apps[3].entry = '//routing.client.sae.com';
+    apps[4].entry = '//authorize.client.sae.com';
     apps.push({
-        name: 'test', // 唯一 id
+        name: 'user', // 唯一 id
         entry: '//localhost:8000', // html entry
-        path: "/test",
+        path: "/user",
     });
     appConfig.siteConfig = {
         ...appConfig.siteConfig,
