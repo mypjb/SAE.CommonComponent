58141eca834e7403486428805ee9dcaa4049c15d
mypjb <280417314@qq.com>
2021-07-22 22:33:18 +0800

    [client] Fix oauth scope error

--- clients/OAuth/src/pages/index.tsx
+++ clients/OAuth/src/pages/index.tsx
@@ -5,16 +5,18 @@ import { useModel } from 'umi';
 export default (props) => {
 
   const { siteConfig } = useModel('@@initialState').initialState?.masterProps.masterState;
-  const { authority, appId, redirectUris, postLogoutRedirectUris } = siteConfig.oauth;
+  const { authority, appId, redirectUris, postLogoutRedirectUris, scope } = siteConfig.oauth;
 
   const oidcConfig = {
     authority: authority,
     client_id: appId,
     redirect_uri: Array.isArray(redirectUris) ? redirectUris[0] : redirectUris,
     response_type: "id_token token",
-    scope: "openid profile " + siteConfig.scope,
+    scope: "openid profile " + scope,
     post_logout_redirect_uri: Array.isArray(postLogoutRedirectUris) ? postLogoutRedirectUris[0] : postLogoutRedirectUris
   };
+
+  console.log(oidcConfig);
 
   const mgr = new UserManager(oidcConfig);
 
