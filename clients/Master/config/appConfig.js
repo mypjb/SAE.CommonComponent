66ce3d19b875886231e50b3bedd57f04a378bc82
mypjb <280417314@qq.com>
2024-03-18 20:42:58 +0800

    [initial] modify development sitemap data

--- clients/Master/config/appConfig.js
+++ clients/Master/config/appConfig.js
@@ -6,7 +6,7 @@ const userKey = "sae_user";
 
 const clusterKey = "_cluster_selected_key_";
 
-let configUrl = "http://api.sae.com/appdata/public?appid=uX16DIGAhUWYdXvp5mzKWQ&env=Production";
+let configUrl = "http://api.sae.com/appdata/public?appid=kkvrFe4ZKkGQj65j2-01xA&env=Production";
 
 
 const storage = window.localStorage;
@@ -21,7 +21,7 @@ export const appConfig = {
 };
 
 if (ENV == "dev") {
-    configUrl = "http://localhost:8080/appdata/public?appid=uX16DIGAhUWYdXvp5mzKWQ&env=Development";
+    configUrl = "http://localhost:8080/appdata/public?appid=kkvrFe4ZKkGQj65j2-01xA&env=Development";
 }
 
 export const userManager = {
