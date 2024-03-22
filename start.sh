1609e449eaab805dc7b57b96e701a998ebab251b
mypjb <280417314@qq.com>
2024-03-22 19:24:43 +0800

    [shell] add start stop shell

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dotnet run --project src/SAE.CommonComponent.Master/SAE.CommonComponent.Master.csproj -c Debug --urls "http://0.0.0.0:8080" -e "ASPNETCORE_ENVIRONMENT=Development" &
+
+clients/start.sh
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ps -ef|grep dotnet |grep -v grep|awk '{print $2}' |xargs kill
+
+./clients/stop.sh
